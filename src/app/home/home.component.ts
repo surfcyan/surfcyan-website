a3618f4bedb1595fd8f45543ca60922ae49eae1e
Kaerik Gautam <gautam.kartik05@gmail.com>
2020-11-27 23:37:01 +0530

    video playback error fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @HostListener('window:scroll', ['$event']) private onScroll($event: Event): void {
+    var scrollY = $event['target']['defaultView']['scrollY']
+    var screenHeight = window.screen.height;
+    if(scrollY>(screenHeight/2-100)){
+      document.getElementById('bg').classList.add('bgColor')
+    }
+    else{
+      document.getElementById('bg').classList.remove('bgColor')
+    }
+  };
+
+  constructor() {
+
+  }
   audioPlayer: HTMLMediaElement
   ngOnInit(): void {
     this.audioPlayer = document.getElementById('vid') as HTMLMediaElement;
     console.log(this.audioPlayer)
-    // this.audioPlayer.play();
+    this.audioPlayer.muted = true;
+    this.audioPlayer.play();
+    this.audioPlayer.loop = true;
+
   }
 
 }
